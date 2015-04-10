7ebd80a25513a3c777899f61df33c8d362cffd11
David Berner <david@tensqft.net>
2015-04-10 13:59:24 -0400

    testing foodcritic

--- cookbooks/dcb_test/metadata.rb
+++ cookbooks/dcb_test/metadata.rb
@@ -1,5 +1,5 @@
 name             'dcb_test'
-maintainer       ''
+maintainer	 'YOUR_COMPANY_NAME'
 maintainer_email 'dcbaok@tensqft.net'
 license          'All rights reserved'
 description      'Installs/Configures dcb_test'
