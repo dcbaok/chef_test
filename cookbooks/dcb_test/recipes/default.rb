4ac2a766f9bab7016cefb8c11ee06dcbb0057966
David Berner <david@tensqft.net>
2015-04-10 13:09:07 -0400

    git commit for testing chef repo

---
+++ cookbooks/dcb_test/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: dcb_test
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "flawed" do
+   action :nothing
+end
+end
