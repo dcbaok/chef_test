1ea6628ab3d7347a5391c0590bef32a6c5375438
David Berner <david@tensqft.net>
2015-05-14 17:52:22 -0400

    continuing to debug deploy

--- test/support/manage_deploy.sh
+++ test/support/manage_deploy.sh
@@ -8,11 +8,11 @@ if [ "$TRAVIS_BRANCH" == 'master' ] ; then
 
   # place user pem key for chef.io
   echo "Placing user PEM"
-  openssl aes-256-cbc -pass env:TRAVIS_PEM_PW -in $CHEFDIR/$OPSCODE_USER.pem.enc -out $CHEFDIR/$OPSCODE_USER.pem -d ;
+  openssl aes-256-cbc -pass env:chef_validator_pw -in $CHEFDIR/$OPSCODE_USER.pem.enc -out $CHEFDIR/$OPSCODE_USER.pem -d ;
 
   # place org validator pem key for chef.io
   echo "Placing org validator PEM"
-  openssl aes-256-cbc -pass env:TRAVIS_PEM_PW -in $CHEFDIR/$ORGNAME.pem.enc -out $CHEFDIR/$ORGNAME-validator.pem -d ;
+  openssl aes-256-cbc -pass env:chef_validator_pw -in $CHEFDIR/$ORGNAME.pem.enc -out $CHEFDIR/$ORGNAME-validator.pem -d ;
 
   bundle exec rake deploy ;
 fi
